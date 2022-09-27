403b36c5095d186d935ad0be0ed2aeaeed8322be
VB Private <vladabuba@gmail.com>
2022-09-27 22:06:09 +0200

    Added engine for tasks.

---
+++ thrnetapp1/thrnetapp1/Engine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thrnetapp1
+{
+    public class Engine
+    {
+        public Engine()
+        {
+
+        }
+
+        public void DoWork()
+        {
+            Task T1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("This is from worker thread.");
+            });
+
+            Task T2 = T1.ContinueWith(t =>
+            {
+
+                Console.WriteLine("Back on main thread.");
+
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+    }
+}
--- thrnetapp1/thrnetapp1/Program.cs
+++ thrnetapp1/thrnetapp1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using thrnetapp1;
+
 Console.WriteLine("Hello, World!");
+
+Engine e = new Engine();
+e.DoWork();
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+Console.WriteLine("Done work.");
